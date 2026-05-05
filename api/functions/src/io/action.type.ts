eb40f44e5caa1429c89bdfa82f7a20e70e3ea71c
andrijafriganovic <andrijafriganovic@gmail.com>
2026-05-05 20:20:45 +0000

    feat: :fire: remove action response body concept

--- api/functions/src/io/action.type.ts
+++ api/functions/src/io/action.type.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from "express";
 
 type ActionBody<T> = {
   action: { name: string };
-  input: { args: T };
+  input: { arg1: T };
   session_variables: {
     "x-hasura-role": string;
     "x-hasura-user-id": string;
@@ -10,10 +10,3 @@ type ActionBody<T> = {
 };
 
 export type Action<T> = Request<any, any, ActionBody<T>>;
-
-export type ActionResponseBody<T> = {
-  data?: T;
-  error?: any;
-};
-
-export type ActionResponse<T = undefined> = Response<ActionResponseBody<T>>;
